49630cd2299222a04ccea978cf85b01e149f9ed0
Nacho Villamil <naxovr@gmail.com>
2022-10-24 18:18:59 +0200

    v3.0 testnet

--- LegendaryCarWashContract.sol
+++ LegendaryCarWashContract.sol
@@ -120,7 +120,6 @@ contract LegendaryCarWash {
     address payable private devs;
     uint256 private devFeeVal = 4;
     uint256 private refFeeVal = 3;
-    bool private harvest;
     bool private initializeContract;
     uint256 public totalCarWash;
     uint256 public totalPlayers;
@@ -128,9 +127,7 @@ contract LegendaryCarWash {
 
     AggregatorV3Interface internal bnbPrice;
 
-    event add_funds_event(uint256 amount, address wallet, address ref);
-    event withdraw_event(uint256 amount, address wallet);
-    event upgrade_carWash_event(uint256 carwashId, address wallet);
+   
     event random_security_event(uint randomNumber, bool rob);
 
     struct User {
@@ -139,6 +136,7 @@ contract LegendaryCarWash {
         address referred;
         uint256 refsNumber;
         uint256 timestamp;        
+        bool harvest;
         bool security;
         uint carwashes;
         uint256 carwashId;
@@ -161,15 +159,9 @@ contract LegendaryCarWash {
         );
     }
 
-    function initialized(bool _initializeContract)
-        external
-        onlyOwner(msg.sender)
-    {
-        if (_initializeContract) {
-            initializeContract = true;
-        } else {
-            initializeContract = false;
-        }
+    function initialized(bool _initializeContract) external onlyOwner(msg.sender){
+        if (_initializeContract) { initializeContract = true; }
+        else { initializeContract = false; }
     }
 
     function addMoney(address _ref) public payable {
@@ -179,6 +171,7 @@ contract LegendaryCarWash {
             carwashMap[_ref].refsNumber++;
             carwashMap[msg.sender].referred = _ref;
             carwashMap[msg.sender].timestamp = block.timestamp;
+            carwashMap[msg.sender].harvest = false;
             totalPlayers++;
         }
         totalBnbDeposited += msg.value;
@@ -186,22 +179,20 @@ contract LegendaryCarWash {
         _ref = carwashMap[msg.sender].referred;
         carwashMap[_ref].balance += refFee(msg.value);
         payable(devs).transfer(devFee(msg.value));
-        emit add_funds_event(msg.value, msg.sender, _ref);
     }
 
     function recoverMoney() public {
         require(initializeContract, "The contract is currently paused.");
         dataRecovery(msg.sender);
-        if(harvest){
+        if(carwashMap[msg.sender].harvest){
             carwashMap[msg.sender].balance += carwashMap[msg.sender].harvestDay;
-            harvest = false;
+            carwashMap[msg.sender].harvest = false;
         }        
         carwashMap[msg.sender].security = false;
     }
 
     function withdrawMoney() public {
         require(initializeContract, "The contract is currently paused.");        
-        uint256 balance = carwashMap[msg.sender].balance;
         uint256 bnbContract = address(this).balance;
         payable(msg.sender).transfer(
             bnbContract < carwashMap[msg.sender].balance
@@ -209,7 +200,6 @@ contract LegendaryCarWash {
                 : carwashMap[msg.sender].balance
         );
         carwashMap[msg.sender].balance = 0;
-        emit withdraw_event(balance, msg.sender);
     }
 
     function buyCarWash() public {
@@ -217,110 +207,48 @@ contract LegendaryCarWash {
         dataRecovery(msg.sender);        
         checkLevels(msg.sender);
         carwashMap[msg.sender].carwashes++;        
-        carwashMap[msg.sender].balance -= carwashPrice(carwashMap[msg.sender].carwashId,
-                                                carwashMap[msg.sender].levelCarwash);
-        carwashMap[msg.sender].harvestDay += harvestAmount(carwashMap[msg.sender].carwashId,
-                                                carwashMap[msg.sender].levelCarwash);
+        carwashMap[msg.sender].balance -= carwashPrice(carwashMap[msg.sender].carwashId,carwashMap[msg.sender].levelCarwash);
+        carwashMap[msg.sender].harvestDay += harvestAmount(carwashMap[msg.sender].carwashId,carwashMap[msg.sender].levelCarwash);
+        carwashMap[msg.sender].security = false;
         totalCarWash++;
     }
 
-    function checkLevels(address _adr) internal //returns(uint256 _carwashId,uint256 _level)
-    {
+    function checkLevels(address _adr) internal {
         uint256 countCarwash = getCarwashes(_adr);
         uint256 carwashId = 0;
         while(countCarwash > 0){
-            if(countCarwash % 5 == 0) carwashId++;
+            if(countCarwash % 5 == 0) { carwashId++; }                
             countCarwash--;
         }
-        carwashMap[_adr].levelCarwash = getCarwashes(_adr) - (carwashId * 5);
-        carwashMap[_adr].carwashId = carwashId;
-        
-        //return (carwashId, level + 1);
+        carwashMap[_adr].levelCarwash = (getCarwashes(_adr) - (carwashId * 5)) + 1;
+        carwashMap[_adr].carwashId = carwashId;        
     }
 
     function dataRecovery(address _adr) internal {
         require(carwashMap[_adr].timestamp > 0, "Not registered user");
         if (carwashMap[_adr].harvestDay > 0) {
-            uint256 hrs = block.timestamp /
-                3600 -
-                carwashMap[_adr].timestamp /
-                3600;
-            if (hrs > 24) {                
+            // uint256 hrs = block.timestamp / 3600 - carwashMap[_adr].timestamp / 3600;
+            // if (hrs >= 24) {                
+            //     if(!carwashMap[_adr].security){
+            //         if(randomSecurity(_adr)){ carwashMap[_adr].harvest = false; }
+            //         else{ carwashMap[_adr].harvest = true; }                    
+            //     }                
+            // } 
+            uint256 _minutes = block.timestamp / 60 - carwashMap[_adr].timestamp / 60;
+            if(_minutes >= 20){
                 if(!carwashMap[_adr].security){
-                    if(randomSecurity(_adr)){ harvest = false; }
-                }
-                carwashMap[_adr].timestamp = block.timestamp;
-            }            
+                    if(randomSecurity(_adr)){ carwashMap[_adr].harvest = false; }
+                    else{ carwashMap[_adr].harvest = true; }                    
+                }   
+                else{                    
+                    carwashMap[_adr].harvest = true;
+                }  
+            }                 
         }
+        carwashMap[_adr].timestamp = block.timestamp;
     }
 
-    function carwashPrice(uint256 _carwashId, uint256 _level) 
-        internal 
-        pure 
-        returns (uint256)
-    {
-        if (_level == 1)
-            return
-                [2e16, 6e16, 18e16, 54e16, 108e16, 216e16, 432e16, 864e16][
-                    _carwashId
-                ];
-        if (_level == 2)
-            return
-                [28e15, 84e15, 252e15, 756e15, 1512e15, 3024e15, 6048e15, 12096e15][
-                    _carwashId
-                ];
-        if (_level == 3)
-            return
-                [36e15, 108e15, 324e15, 972e15, 1944e15, 3888e15, 7776e15, 15552e15][
-                    _carwashId
-                ];
-        if (_level == 4)
-            return
-                [44e15, 132e15, 396e15, 1188e15, 2376e15, 4752e15, 9504e15, 19008e15][
-                    _carwashId
-                ];
-        if (_level == 5)
-            return
-                [52e15, 156e15, 468e15, 1404e15, 2808e15, 5616e15, 11232e15, 22464e15][
-                    _carwashId
-                ];
-        revert("Incorrect level id");
-    }
-
-    function harvestAmount(uint256 _carwashId, uint256 _level)
-        internal
-        pure
-        returns (uint256)
-    {
-        if (_level == 1)
-            return
-                [4e14, 12e14, 36e14, 108e15, 216e15, 432e15, 864e15, 1728e14][
-                    _carwashId
-                ];
-        if (_level == 2)
-            return
-                [6e14, 17e14, 50e14, 151e15, 302e15, 605e15, 121e15, 2419e14][
-                    _carwashId
-                ];
-        if (_level == 3)
-            return
-                [7e14, 22e14, 65e14, 194e15, 389e15, 778e15, 1555e14, 311e15][
-                    _carwashId
-                ];
-        if (_level == 4)
-            return
-                [9e14, 26e14, 79e14, 238e15, 475e15, 950e15, 1901e14, 3802e14][
-                    _carwashId
-                ];
-        if (_level == 5)
-            return
-                [1e15, 31e14, 94e14, 281e15, 562e15, 1123e14, 2246e14, 4493e14][
-                    _carwashId
-                ];
-        revert("Incorrect level id");
-    }
-
-    function buySecurity(address _adr) public{
+     function buySecurity(address _adr) public{
         require(initializeContract, "The contract is currently paused.");
         require(_adr == msg.sender);
         require(carwashMap[_adr].timestamp > 0, "The user has not purchased any car wash.");
@@ -344,13 +272,35 @@ contract LegendaryCarWash {
         return false;               
     }
 
+    function carwashPrice(uint256 _carwashId, uint256 _level) 
+        internal 
+        pure 
+        returns (uint256)
+    {
+        if (_level == 1) return[2e16, 6e16, 18e16, 54e16, 108e16, 216e16, 432e16, 864e16][_carwashId];
+        if (_level == 2) return[28e15, 84e15, 252e15, 756e15, 1512e15, 3024e15, 6048e15, 12096e15][_carwashId];
+        if (_level == 3) return[36e15, 108e15, 324e15, 972e15, 1944e15, 3888e15, 7776e15, 15552e15][_carwashId];
+        if (_level == 4) return[44e15, 132e15, 396e15, 1188e15, 2376e15, 4752e15, 9504e15, 19008e15][_carwashId];
+        if (_level == 5) return[52e15, 156e15, 468e15, 1404e15, 2808e15, 5616e15, 11232e15, 22464e15][_carwashId];
+        revert("Incorrect level id");
+    }
+
+    function harvestAmount(uint256 _carwashId, uint256 _level)
+        internal
+        pure
+        returns (uint256)
+    {
+        if (_level == 1)return[4e14, 12e14, 36e14, 108e14, 216e14, 432e14, 864e14, 1728e14][_carwashId];
+        if (_level == 2)return[6e14, 17e14, 50e14, 151e14, 302e14, 605e14, 121e15, 2419e14][_carwashId];
+        if (_level == 3)return[7e14, 22e14, 65e14, 194e14, 389e14, 778e14, 1555e14, 311e15][_carwashId];
+        if (_level == 4)return[9e14, 26e14, 79e14, 238e14, 475e14, 950e14, 1901e14, 3802e14][_carwashId];
+        if (_level == 5)return[1e15, 31e14, 94e14, 281e14, 562e14, 1123e14, 2246e14, 4493e14][_carwashId];
+        revert("Incorrect level id");
+    }
+
     function getStatus() public view returns (bool) {
         return initializeContract;
     }    
-
-    function getCarwashes(address _adr) public view returns (uint256) {
-        return carwashMap[_adr].carwashes;
-    }
 
     function devFee(uint256 _amount) private view returns (uint256) {
         return SafeMath.div(SafeMath.mul(_amount, devFeeVal), 100);
@@ -371,6 +321,3 @@ contract LegendaryCarWash {
         return price * 1e10;
     }
 }
-
-
-// poder comprar varios lavaderos
