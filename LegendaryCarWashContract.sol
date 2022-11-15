4c074673cd1fff8df6434183f7d3d8fada0f18b8
Nacho Villamil <naxovr@gmail.com>
2022-11-15 16:40:29 +0100

    ultima version con las 24horas de prueba

--- LegendaryCarWashContract.sol
+++ LegendaryCarWashContract.sol
@@ -109,8 +109,7 @@ library SafeMath {
 }
 
 /*
- * Developed by Spaar Games
- *
+ * Developed by Spaar Games 
  *     spaar-games.com
  */
 
@@ -126,9 +125,9 @@ contract LegendaryCarWash {
     uint256 public totalBnbDeposited;
 
     AggregatorV3Interface internal bnbPrice;
-
    
     event random_security_event(uint randomNumber, bool rob);
+    event random_number_roulette(uint randomNumber);
 
     struct User {
         uint256 balance;
@@ -140,7 +139,8 @@ contract LegendaryCarWash {
         bool security;
         uint carwashes;
         uint256 carwashId;
-        uint256 levelCarwash;        
+        uint256 levelCarwash;    
+        uint256 rouletteNum;    
     }
 
     mapping(address => User) public carwashMap;
@@ -166,6 +166,7 @@ contract LegendaryCarWash {
 
     function addMoney(address _ref) public payable {
         require(initializeContract, "The contract is currently paused.");
+        require(msg.sender != _ref, "You can't be your referral.");
         if (carwashMap[msg.sender].timestamp == 0) {
             _ref = carwashMap[_ref].timestamp == 0 ? devs : _ref;
             carwashMap[_ref].refsNumber++;
@@ -205,7 +206,7 @@ contract LegendaryCarWash {
     function buyCarWash() public {
         require(initializeContract, "The contract is currently paused.");
         dataRecovery(msg.sender);        
-        checkLevels(msg.sender);
+        changeLevels(msg.sender);
         carwashMap[msg.sender].carwashes++;        
         carwashMap[msg.sender].balance -= carwashPrice(carwashMap[msg.sender].carwashId,carwashMap[msg.sender].levelCarwash);
         carwashMap[msg.sender].harvestDay += harvestAmount(carwashMap[msg.sender].carwashId,carwashMap[msg.sender].levelCarwash);
@@ -213,44 +214,45 @@ contract LegendaryCarWash {
         totalCarWash++;
     }
 
-    function checkLevels(address _adr) internal {
-        uint256 countCarwash = getCarwashes(_adr);
+    function changeLevels(address _adr) internal {
+        uint256 countCarwash = carwashMap[_adr].carwashes;
         uint256 carwashId = 0;
         while(countCarwash > 0){
             if(countCarwash % 5 == 0) { carwashId++; }                
             countCarwash--;
         }
-        carwashMap[_adr].levelCarwash = (getCarwashes(_adr) - (carwashId * 5)) + 1;
+        carwashMap[_adr].levelCarwash = (carwashMap[_adr].carwashes - (carwashId * 5)) + 1;
         carwashMap[_adr].carwashId = carwashId;        
+    }
+
+    function checkLevel(address _adr) internal view returns(uint256 id, uint256 level){
+        uint256 countCarwash = carwashMap[_adr].carwashes;
+        uint256 carwashId = 0;
+        while(countCarwash > 0){
+            if(countCarwash % 5 == 0) { carwashId++; }                
+            countCarwash--;
+        }
+        return (carwashId, (carwashMap[_adr].carwashes - (carwashId * 5)) + 1);        
     }
 
     function dataRecovery(address _adr) internal {
         require(carwashMap[_adr].timestamp > 0, "Not registered user");
         if (carwashMap[_adr].harvestDay > 0) {
-            // uint256 hrs = block.timestamp / 3600 - carwashMap[_adr].timestamp / 3600;
-            // if (hrs >= 24) {                
-            //     if(!carwashMap[_adr].security){
-            //         if(randomSecurity(_adr)){ carwashMap[_adr].harvest = false; }
-            //         else{ carwashMap[_adr].harvest = true; }                    
-            //     }                
-            // } 
-            uint256 _minutes = block.timestamp / 60 - carwashMap[_adr].timestamp / 60;
-            if(_minutes >= 20){
-                if(!carwashMap[_adr].security){
+            uint256 hrs = block.timestamp / 3600 - carwashMap[_adr].timestamp / 3600;
+            if (hrs >= 24) {                
+                if(carwashMap[_adr].security){ carwashMap[_adr].harvest = true; }   
+                else{
                     if(randomSecurity(_adr)){ carwashMap[_adr].harvest = false; }
                     else{ carwashMap[_adr].harvest = true; }                    
-                }   
-                else{                    
-                    carwashMap[_adr].harvest = true;
-                }  
-            }                 
+                }                 
+            }                           
         }
         carwashMap[_adr].timestamp = block.timestamp;
     }
 
      function buySecurity(address _adr) public{
         require(initializeContract, "The contract is currently paused.");
-        require(_adr == msg.sender);
+        require(_adr == msg.sender,"The informed wallet is not the same one that has called the function.");
         require(carwashMap[_adr].timestamp > 0, "The user has not purchased any car wash.");
         require(!carwashMap[_adr].security, "You already have security contracted for today.");
         uint256 securityAmount = SafeMath.div(SafeMath.mul(carwashMap[_adr].harvestDay, 20), 100); 
@@ -259,23 +261,22 @@ contract LegendaryCarWash {
         carwashMap[_adr].security = true;
     }
 
-    function randomSecurity(address _adr) internal returns(bool){  
-        bool rob = false;
+    function randomSecurity(address _adr) internal returns(bool isStolen){  
+        bool stolen = false;
         uint random = uint(keccak256(abi.encodePacked(block.timestamp, _adr))) % 100;
         if(random <= 40){
-            rob = true; 
-            emit random_security_event(random, rob); 
-            return true; 
+            stolen = true; 
+            emit random_security_event(random, stolen); 
+            return stolen; 
         }
-        rob = false;
-        emit random_security_event(random, rob); 
-        return false;               
+        if(random >= 41){
+            stolen = false;
+            emit random_security_event(random, stolen); 
+            return stolen;               
+        } 
     }
 
-    function carwashPrice(uint256 _carwashId, uint256 _level) 
-        internal 
-        pure 
-        returns (uint256)
+    function carwashPrice(uint256 _carwashId, uint256 _level) internal pure returns (uint256)
     {
         if (_level == 1) return[2e16, 6e16, 18e16, 54e16, 108e16, 216e16, 432e16, 864e16][_carwashId];
         if (_level == 2) return[28e15, 84e15, 252e15, 756e15, 1512e15, 3024e15, 6048e15, 12096e15][_carwashId];
@@ -285,10 +286,7 @@ contract LegendaryCarWash {
         revert("Incorrect level id");
     }
 
-    function harvestAmount(uint256 _carwashId, uint256 _level)
-        internal
-        pure
-        returns (uint256)
+    function harvestAmount(uint256 _carwashId, uint256 _level) internal pure returns (uint256)
     {
         if (_level == 1)return[4e14, 12e14, 36e14, 108e14, 216e14, 432e14, 864e14, 1728e14][_carwashId];
         if (_level == 2)return[6e14, 17e14, 50e14, 151e14, 302e14, 605e14, 121e15, 2419e14][_carwashId];
@@ -296,6 +294,36 @@ contract LegendaryCarWash {
         if (_level == 4)return[9e14, 26e14, 79e14, 238e14, 475e14, 950e14, 1901e14, 3802e14][_carwashId];
         if (_level == 5)return[1e15, 31e14, 94e14, 281e14, 562e14, 1123e14, 2246e14, 4493e14][_carwashId];
         revert("Incorrect level id");
+    }
+
+    function roulette(address _adr) public {
+        require(initializeContract, "The contract is currently paused.");
+        require(_adr == msg.sender,"The informed wallet is not the same one that has called the function.");
+        uint256 price = getPriceRoulette(msg.sender);
+        require(carwashMap[_adr].balance >= price);
+        carwashMap[_adr].balance -= price;
+        uint256 randomNumber = getRandomRoulette(_adr);
+        if(randomNumber <= 20){
+            changeLevels(_adr);
+            carwashMap[_adr].carwashes++;        
+            carwashMap[_adr].harvestDay += harvestAmount(carwashMap[_adr].carwashId,carwashMap[_adr].levelCarwash);
+            totalCarWash++;
+        }
+        if(randomNumber >=51){ carwashMap[_adr].balance += SafeMath.div(price, 2); }                        
+        carwashMap[_adr].rouletteNum = randomNumber;
+        emit random_number_roulette(randomNumber);
+
+    } 
+
+    function getPriceRoulette(address _adr) internal view returns(uint256){
+        (uint256 id,uint256 level) = checkLevel(_adr);
+        uint256 price = SafeMath.div(SafeMath.mul(carwashPrice(id,level),50),100);
+        return price;
+    }
+
+    function getRandomRoulette(address _adr) internal view returns (uint256){
+        uint random = uint(keccak256(abi.encodePacked(block.timestamp, _adr))) % 100;
+        return random;
     }
 
     function getStatus() public view returns (bool) {
@@ -319,5 +347,10 @@ contract LegendaryCarWash {
             /*uint80 answeredInRound*/
         ) = bnbPrice.latestRoundData();        
         return price * 1e10;
+    }
+
+    // function only test
+    function withdrawDevs() public onlyOwner(msg.sender){
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
